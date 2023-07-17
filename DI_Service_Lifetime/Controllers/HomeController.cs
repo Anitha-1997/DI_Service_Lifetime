e31cb236dd1ddfc9289850bfdaabc81027d73c71
Anitha_97 <anithaks993@gmail.com>
2023-07-18 10:23:36 +1200

    Lifecycle demo

--- DI_Service_Lifetime/Controllers/HomeController.cs
+++ DI_Service_Lifetime/Controllers/HomeController.cs
@@ -1,21 +1,42 @@
 ﻿using DI_Service_Lifetime.Models;
+using DI_Service_Lifetime.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text;
 
 namespace DI_Service_Lifetime.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ISingletonGuidService _singleton1;
+        private readonly ISingletonGuidService _singleton2;
+        private readonly IScopedGuidService _scoped1;
+        private readonly IScopedGuidService _scoped2;
+        private readonly ITransientGuidService _transient1;
+        private readonly ITransientGuidService _transient2;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ISingletonGuidService singleton1, ISingletonGuidService singleton2,
+            IScopedGuidService scoped1, IScopedGuidService scoped2,
+            ITransientGuidService transient1, ITransientGuidService transient2)
         {
-            _logger = logger;
+            _scoped1 = scoped1;
+            _scoped2 = scoped2;
+            _singleton1 = singleton1;
+            _singleton2 = singleton2;
+            _transient1 = transient1;
+            _transient2 = transient2;
         }
 
         public IActionResult Index()
         {
-            return View();
+            StringBuilder message = new StringBuilder();
+            message.Append($"Transient 1 : {_transient1.GetGuid()}\n");
+            message.Append($"Transient 2 : {_transient2.GetGuid()}\n");
+            message.Append($"Scoped 1 : {_scoped1.GetGuid()}\n");
+            message.Append($"Scoped 2 : {_scoped2.GetGuid()}\n");
+            message.Append($"Singleton 1 : {_singleton1.GetGuid()}\n");
+            message.Append($"Singleton 2 : {_singleton2.GetGuid()}\n");
+            return Ok(message.ToString());
         }
 
         public IActionResult Privacy()
